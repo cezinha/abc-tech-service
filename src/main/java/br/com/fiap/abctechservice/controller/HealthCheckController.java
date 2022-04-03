f659d22d359d1d9113cd411ea283e6a4bdde8b5f
Celina Uemura (cezinha) <celina.uemura@gmail.com>
2022-04-03 08:34:00 -0300

    buildspec + version

--- src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
@@ -12,5 +16,18 @@ public class HealthCheckController {
     @GetMapping()
     public ResponseEntity<Object> status() {
         return ResponseEntity.ok("<h1>Hello world!</h1>");
+    }
+
+    @GetMapping("version")
+    public ResponseEntity<Object> version() {
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
     }
 }
